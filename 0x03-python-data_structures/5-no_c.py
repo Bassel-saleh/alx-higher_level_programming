a57a1e66744f8a56c9c0e2d3798c3490b03a7557
Bassel-saleh <basselh26@gmai.com>
2023-12-03 17:08:46 +0200

    debugging

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     noc_str = ""
     for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
             noc_str += my_string[i]
     return noc_str
