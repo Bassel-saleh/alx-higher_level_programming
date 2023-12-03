0edb5da28325866ccbed5a671fe32d5c6c071467
Bassel-saleh <basselh26@gmai.com>
2023-12-03 19:15:25 +0200

    debugging

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == 0:
-        c_tuplei = (None)
+        c_tuplei = (0, None)
         return(c_tuple)
     else:
         c_tuple = (len(sentence), sentence[0])
