80a02993208fa3b98ff27e8143c3e60c7147688c
Bassel-saleh <basselh26@gmai.com>
2023-12-03 17:02:19 +0200

    added task#5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    noc_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            noc_str += my_string[i]
+    return noc_str
