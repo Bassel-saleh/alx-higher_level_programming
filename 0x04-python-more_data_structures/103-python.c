748114e7227cde713f43876429db9e316e82b795
Bassel-saleh <basselh26@gmai.com>
2023-12-07 04:28:44 +0200

    added task#16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,2 +1,56 @@
 #include <python.h>
+/**
+ * print_python_list - print info about python list
+ * @p: PyObject list
+ */
+void print_python_list(PyObject *p)
+{
+	int size_p, alloc_p, j;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
+	size_p = var->ob_size;
+	alloc_p = list->allocated;
+	printf("[*] Python list info\n");
+	printf("[*] size of the Python List = %d\n", size_p);
+	printf("[*] Allocated = %d\n", alloc_p);
+	for (j = 0; i < size_p; i++)
+	{
+		type = list->ob_item[j]->ob_type->tp_name;
+		printf("Element %d: %s\n", j, type);
+		if (strcmp(type, "bytes") == 0)
+			print_bytes(list->ob_item[j]);
+	}
+}
+
+/**
+ * print_python_bytes - print info about python byte objects
+ * @p: PyObject byte object
+ */
+void print_python_bytes(PyObject *p)
+{
+	unsigned char j, size_p;
+	PyBytesObject *bytes = (PyBytesObject *)p;
+
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] invalid Bytes Object\n");
+	}
+	printf("  size: %1d\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+	if (((PyVarObject *)p)->ob_size > 10)
+		size_p = 10;
+	else
+		size_p = ((PyVarObject *)p)->ob_size + 1;
+	printf("  first %d bytes: ", size_p);
+	for (j = 0; j < size_p; j++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (j == (size_p - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
