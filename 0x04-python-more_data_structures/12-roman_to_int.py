a9e5fa18afefd96e406eeca25af3f402f2e3bbac
Bassel-saleh <basselh26@gmai.com>
2023-12-07 02:59:56 +0200

    debugging

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     t = 0
     n = 0
-    r_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+    r_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for x in (roman_string):
         n = r_digits[x]
         t += n
