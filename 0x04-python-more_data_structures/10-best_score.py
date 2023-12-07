2ede2ac727a1ffe153a332b4dae704ed636178d8
Bassel-saleh <basselh26@gmai.com>
2023-12-07 02:33:55 +0200

    debugging

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_v = 0
-    max_key = ""
+    max_k = ""
     for k, v in a_dictionary.items():
         if v > max_v:
             max_v = v
