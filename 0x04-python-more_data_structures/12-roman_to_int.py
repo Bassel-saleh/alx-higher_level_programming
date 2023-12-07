75ccc6e26db8379f9c3d3d2822739f2a4dde7f02
Bassel-saleh <basselh26@gmai.com>
2023-12-07 02:58:31 +0200

    added task#12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3'
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    t = 0
+    n = 0
+    r_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+    for x in (roman_string):
+        n = r_digits[x]
+        t += n
+    return t
