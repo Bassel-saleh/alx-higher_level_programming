430f65fdb7ace38ff569e9255f215055f9fb8029
Bassel-saleh <basselh26@gmai.com>
2023-12-19 18:17:18 +0200

    added task#4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """property for the length of a Square"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """setter for the length of square"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+    def area(self):
+        """Area of square"""
+        return self.__size ** 2
