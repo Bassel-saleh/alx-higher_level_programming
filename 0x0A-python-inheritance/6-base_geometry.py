c0ed2bbd00b0ac93bafe0e6ac711b851d861bab4
Bassel-saleh <basselh26@gmai.com>
2024-01-08 22:53:48 +0200

    added task#6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for BaseGeometry class.'''
+
+
+class BaseGeometry:
+    '''BaseGeometry class'''
+    def area(self):
+        raise Exception('area() is not implemented')
