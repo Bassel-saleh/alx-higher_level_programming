94747f7a797d653fb4d46e6a2692c3d282a89bec
Bassel-saleh <basselh26@gmai.com>
2024-03-17 22:58:47 +0200

    debugging

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" connect class to database """
+import sys
+from model_state import base, state
+from sqlalchemy import (create_engine)
+if __name__ == "__main__":
+    start = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(start)
