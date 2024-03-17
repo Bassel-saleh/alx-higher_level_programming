cdac243a4b3e312fc5596af8781f5c5c15080207
Bassel-saleh <basselh26@gmai.com>
2024-03-18 00:19:51 +0200

    added task#14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" prints the State object with the argument from database """
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    start = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(start)
+    Session = sessionmaker(bind=start)
+    session = Session()
+    q = (State.name, City.id, City.name).filter(State.id == City.state_id)
+    for kd in (session.query(q).filter(State.id == City.state_id)):
+        print(kd[0] + ": (" + str(kd[1]) + ") " + kd[2])
