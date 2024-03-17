3159b229c3af41b360613d05cbdea0ebbcb07ed2
Bassel-saleh <basselh26@gmai.com>
2024-03-18 00:36:11 +0200

    debugging

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
