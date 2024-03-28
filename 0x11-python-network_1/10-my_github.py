f46fcf694075ac6294229e36660cd4b4340cbcc7
Bassel-saleh <basselh26@gmai.com>
2024-03-28 10:10:24 +0200

    added task#9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    passcode = sys.argv[2]
+    url = "https://api.github.com/user"
+    auth = (usr, passcode)
+    rspns = requests.get(url, auth=auth)
+    if rspns.status_code == 200:
+        usr_info = rspns.json()
+        usr_id = usr_info['id']
+        print(usr_id)
+    else:
+        print(None)
