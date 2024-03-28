28a945ffb01fcd991d977f6f07e82449a65e27b1
Bassel-saleh <basselh26@gmai.com>
2024-03-28 09:20:57 +0200

    added task#3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes in a URL
+Sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+"""
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as rspns:
+            data = rspns.read().decode('utf-8')
+            print(data)
+    except urllib.error.HTTPError as error:
+        print("Error code:", error.code)
